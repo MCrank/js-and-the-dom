c331f17506188e36bbe9acd8ef3e409512e7ef95
MCrank <marco_mcse@hotmail.com>
2018-08-16 23:06:28 -0500

    Finished CH 4

--- 4-responding-to-user-interaction/vid-3-delaying-execution-setTimeout/start/temp.js
+++ 4-responding-to-user-interaction/vid-3-delaying-execution-setTimeout/start/temp.js
@@ -1,8 +1,7 @@
-
-function exec(func, arg) {
-  func(arg);
-}
-
-exec((something) => {
-  console.log(something);
-}, 'Greetings, everyone!');
+window.setTimeout(
+  something => {
+    console.log(something);
+  },
+  3000,
+  'Greetings, everyone!'
+);
